d543e296feeb2df77c69266dc15cf845de3feed2
Abdul Munim <iam@munim.net>
2024-11-07 16:30:45 +0100

    Added script to copy backup files after a specific date

---
+++ scripts/copy_log_files_from_backup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Check for the correct number of arguments
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <source_path> <destination_path> <start_date> [end_date]"
+    echo "Date format: YYYY-MM-DD"
+    exit 1
+fi
+
+# Define source and destination paths from arguments
+SOURCE_PATH="$1"
+DESTINATION_PATH="$2"
+START_DATE="$3"
+END_DATE="$4"
+
+# Convert start date to seconds since epoch
+START_EPOCH=$(date -d "$START_DATE" +%s)
+
+# If no end date is provided, set end date to a future date
+if [ -z "$END_DATE" ]; then
+    END_EPOCH=$(date +%s) # Use current date
+else
+    END_EPOCH=$(date -d "$END_DATE" +%s)
+fi
+
+# Generate the include patterns
+INCLUDE_PATTERNS=""
+for ((d = 0; d <= ($END_EPOCH - $START_EPOCH) / 86400; d++)); do
+    CURRENT_DATE=$(date -d "$START_DATE + $d days" +%Y-%m-%d)
+    INCLUDE_PATTERNS+="--include=*-${CURRENT_DATE}.log.7z "
+done
+
+# Final rsync command
+echo ${INCLUDE_PATTERNS}
+#rsync -av --include='*/' $INCLUDE_PATTERNS --exclude='*' "$SOURCE_PATH/" "$DESTINATION_PATH/"
